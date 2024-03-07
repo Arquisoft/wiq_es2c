eeb30d55f9eb0182ce937a64cea1f282adcae55b
Angela <uo284342@uniovi.es>
2024-03-07 14:01:03 +0100

    test pantallaInicio.js

---
+++ webapp/src/components/PantallaInicio.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render, fireEvent, screen, waitFor, act } from '@testing-library/react';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import PantallaInicio from './PantallaInicio';
+import { useUser } from './UserContext';
+
+const { usernameGlobal } = useUser();
+
+const mockAxios = new MockAdapter(axios);
+
+describe('PantallaInicio component', () => {
+  beforeEach(() => {
+    mockAxios.reset();
+  });
+
+  it('muestra la pantalla de inicio correctamente', async () => {
+    render(<Router>
+      <PantallaInicio />
+    </Router>);
+
+    const textoBienvenida = screen.getByLabelText(/¡BIENVENIDO A WIQ ${usernameGlobal}!/i);
+    const nuevaPartidaButton = screen.getByRole('button', { name: /NUEVA PARTIDA/i });
+
+    // Simulate user input
+    await act(async () => {
+        fireEvent.click(nuevaPartidaButton);
+      });
+
+    expect(textoBienvenida).toBeInTheDocument();
+  });
+
+});
