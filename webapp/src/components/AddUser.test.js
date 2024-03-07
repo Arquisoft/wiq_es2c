f8ec0b37c8af646b491c189783d0fc3a11f90e90
Angela <uo284342@uniovi.es>
2024-03-07 14:25:08 +0100

    añado imports q faltan

--- webapp/src/components/AddUser.test.js
+++ webapp/src/components/AddUser.test.js
@@ -3,6 +3,7 @@ import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import AddUser from './AddUser';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const mockAxios = new MockAdapter(axios);
 
