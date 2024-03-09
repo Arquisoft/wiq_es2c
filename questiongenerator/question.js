0988462e622481573235e52361bfe563ed7942f7
UO289337 <145553440+UO289337@users.noreply.github.com>
2024-03-10 00:51:14 +0100

    Añadida URL de la máquina virtual

--- questiongenerator/question.js
+++ questiongenerator/question.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 // Necesario para poder hacer las peticiones desde Game
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000, http://4.234.8.175:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
