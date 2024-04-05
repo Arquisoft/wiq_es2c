5528a23034b35f475cf7979844b7ca7e5a384cb4
Ruben <UO289337@uniovi.es>
2024-04-05 23:20:19 +0200

    Nuevos test de cobertura de código y eliminación de Partida

--- gatewayservice/gateway-service.test.js
+++ gatewayservice/gateway-service.test.js
@@ -1,6 +1,9 @@
 const request = require('supertest');
 const axios = require('axios');
 const app = require('./gateway-service'); 
+const { createServer } = require('http');
+
+const server = createServer(app);
 
 afterAll(async () => {
     app.close();
@@ -37,4 +40,45 @@ describe('Gateway Service', () => {
     expect(response.statusCode).toBe(200);
     expect(response.body.userId).toBe('mockedUserId');
   });
+
+  // Test /generateQuestion endpoint
+  it('should receive the question', async () => {
+    server.listen(8003);
+  
+    jest.spyOn(axios, 'get').mockResolvedValueOnce({ data: {  responseQuestion: "¿Cual es la capital de España?", responseOptions: ["Madrid", "Barcelona", "Oviedo", "Valladolid"],
+    responseCorrectOption: "Madrid", responseImage: null, question_id:"12345abcdef"} });
+  
+    const response = await request(app).get('/generateQuestion').query({
+      user: 'user',
+      newGame: true,
+      numberOfQuestions: 5
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ responseQuestion: "¿Cual es la capital de España?", responseOptions: ["Madrid", "Barcelona", "Oviedo", "Valladolid"],
+    responseCorrectOption: "Madrid", responseImage: null, question_id:"12345abcdef"});
+
+    jest.restoreAllMocks();
+    server.close(true);
+  });
+
+  it('should update the question', async () => {
+    const updatedQuestion = {
+      _id: '660434f228670016dfcac277',
+      enunciado: '¿Cual es la capital de España?',
+      respuesta_correcta: 'Madrid',
+      respuesta_falsa1: 'Barcelona',
+      respuesta_falsa2: 'Oviedo',
+      respuesta_falsa3: 'Valladolid',
+      __v: 0
+      }
+    jest.spyOn(axios, 'get').mockResolvedValueOnce({ data: { message: "Tiempo de pregunta actualizado exitosamente", updatedQuestion: updatedQuestion } });
+
+    const response = await request(app)
+      .get('/updateQuestion')
+      .send({ message: 'Pregunta actualizada', updatedQuestion: updatedQuestion });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ message: "Tiempo de pregunta actualizado exitosamente", updatedQuestion });
+  })
 });
--- webapp/src/components/PantallaInicio.js
+++ webapp/src/components/PantallaInicio.js
@@ -15,7 +15,7 @@ const PantallaInicio = () => {
     const navigate = useNavigate();
 
     function nuevaPartida() {
-        navigate("/Partida")
+        navigate("/Game");
     }
 
     const logoutUser = async () => {
