59034290f25acf4fe04c506ea6a8bcb1b2067ce8
UO288559 <uo288559@uniovi.es>
2024-04-06 12:18:59 +0200

    Corrección del tiempo tardado en responder

--- gatewayservice/gateway-service.js
+++ gatewayservice/gateway-service.js
@@ -47,18 +47,6 @@ app.post('/adduser', async (req, res) => {
   }
 });
 
-// app.get(`/generateQuestion`, async (req, res) => {
-//   try {
-//     // Forward the add user request to the user service
-//     const URL = generatorUrl + '/generateQuestion?user=' + req.query.user + '&newGame=' + req.query.newGame
-//         + '&numberOfQuestions=' + req.query.numberOfQuestions;
-//     const response = await axios.get(URL);
-//     res.json(response.data);
-//   } catch (error) {
-//     res.status(error.response.status).json({ error: error.response.data.error });
-//   }
-// });
-
 app.get(`/generateQuestion`, async (req, res) => {
   try {
     // Forward the add user request to the user service
@@ -73,7 +61,7 @@ app.get(`/generateQuestion`, async (req, res) => {
 app.get(`/updateQuestion`, async (req, res) => {
   try {
     // Forward the add user request to the user service
-    const response = await axios.get(generatorUrl+'/updateQuestion', req.body);
+    const response = await axios.get(generatorUrl+'/updateQuestion?time=' + req.query.time + "", req.body);
     res.json(response.data);
   } catch (error) {
     res.status(error.response.status).json({ error: error.response.data.error });
