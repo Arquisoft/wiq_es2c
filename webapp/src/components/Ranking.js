f056a704703a20ce71c77639c69643233aa6db2a
David <uo289792@uniovi.es>
2024-04-09 13:19:57 +0200

    Cambiada estructura

--- webapp/src/components/Ranking.js
+++ webapp/src/components/Ranking.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
-import { Container, Box, Typography} from '@mui/material';
+import { Container, Box, Typography, Grid} from '@mui/material';
+import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
 import { useUser } from './UserContext';
 import '../App.css';
 
@@ -35,24 +36,39 @@ const Ranking = () => {
         height: '100vh', 
         width: '100%', 
       }}>
-      {ranking && (
-        <Box>
-          <img src={require('./images/medalla-de-plata.png')} alt="Segundo" />
-          <Typography>{ranking.segundo.ratio}%</Typography>
-        </Box>
-      )}
-      {ranking && (
-        <Box>
-          <img src={require('./images/medalla-de-oro.png')} alt="Primero" />
-          <Typography>{ranking.primero.ratio}%</Typography>
-        </Box>
-      )}
-      {ranking && (
-        <Box>
-          <img src={require('./images/medalla-de-bronce.png')} alt="Tercero" />
-          <Typography>{ranking.tercero.ratio}%</Typography>
-        </Box>
-      )}
+      <Grid container spacing={1} justifyContent="center">
+      {/* Fila con una columna para el primero */}
+      <Grid item xs={12} sm={12} md={4} textAlign="center">
+        {ranking && (
+          <Box>
+            <img src={require('./images/medalla-de-oro.png')} alt="Primero" style={{ width: '50%', maxWidth: '100px', marginBottom: '5px' }} />
+            <HorizontalRuleIcon style={{ color: 'black' }} />
+            <Typography variant="h6" sx={{ marginTop: '5px' }}>{ranking.primero.userId} - {ranking.primero.ratio}%</Typography>
+          </Box>
+        )}
+      </Grid>
+      {/* Fila con dos columnas para el segundo y tercero */}
+      <Grid item container xs={12} spacing={1} justifyContent="center">
+        <Grid item xs={12} sm={6} md={4} textAlign="center">
+          {ranking && (
+            <Box>
+              <img src={require('./images/medalla-de-plata.png')} alt="Segundo" style={{ width: '50%', maxWidth: '100px', marginBottom: '5px' }} />
+              <HorizontalRuleIcon style={{ color: 'black' }} />
+              <Typography variant="h6" sx={{ marginTop: '5px' }}>{ranking.segundo.userId} - {ranking.segundo.ratio}%</Typography>
+            </Box>
+          )}
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} textAlign="center">
+          {ranking && (
+            <Box>
+              <img src={require('./images/medalla-de-bronce.png')} alt="Tercero" style={{ width: '50%', maxWidth: '100px', marginBottom: '5px' }} />
+              <HorizontalRuleIcon style={{ color: 'black' }} />
+              <Typography variant="h6" sx={{ marginTop: '5px' }}>{ranking.tercero.userId} - {ranking.tercero.ratio}%</Typography>
+            </Box>
+          )}
+        </Grid>
+      </Grid>
+    </Grid>
     </Container>
   );
 };
