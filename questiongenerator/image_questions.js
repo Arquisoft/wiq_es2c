db77892fc52671b6e27646d72d1bf5d994382edf
Ruben <UO289337@uniovi.es>
2024-04-14 19:27:57 +0200

    Añadidas preguntas por tematicas al generador de preguntas

--- questiongenerator/image_questions.js
+++ questiongenerator/image_questions.js
@@ -1,25 +1,69 @@
 // Todas las consultas
-var queries = 
-    // pregunta = Imagen de un presidente de EE.UU., opcion = Nombres de presidentes de EE.UU.
-    [`SELECT ?option ?optionLabel ?imageLabel
-    WHERE {
-        ?option wdt:P31 wd:Q5;  
-                    wdt:P39 wd:Q11696;  
-                    wdt:P18 ?imageLabel.   
-        SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }
-    }
-    `,
-    // pregunta = Imagen de un país, opcion = Pais
-    `SELECT DISTINCT ?option ?optionLabel ?imageLabel
-    WHERE {
-      ?option wdt:P31 wd:Q6256;               
-            rdfs:label ?optionLabel;          
-      
-      OPTIONAL { ?option wdt:P18 ?imageLabel. }    
-      FILTER(lang(?optionLabel) = "es")       
-      FILTER EXISTS { ?option wdt:P18 ?imageLabel }
-    }
-    `];
+var queries = {};
+
+queries['Geografia'] = 
+    [
+      // pregunta = Imagen de un país, opcion = Pais
+      `
+      SELECT DISTINCT ?option ?optionLabel ?imageLabel
+      WHERE {
+        ?option wdt:P31 wd:Q6256;               
+              rdfs:label ?optionLabel;          
+        
+        OPTIONAL { ?option wdt:P18 ?imageLabel. }    
+        FILTER(lang(?optionLabel) = "es")       
+        FILTER EXISTS { ?option wdt:P18 ?imageLabel }
+      }
+    `
+  ];
+
+queries['Cultura'] = 
+  [
+      // pregunta = cuadro, opcion -> nombre
+      `
+      SELECT ?optionLabel ?imageLabel
+      WHERE {
+          ?option wdt:P31 wd:Q3305213.   
+          ?option wdt:P18 ?imageLabel.       
+          SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }
+      }
+      LIMIT 200
+      `
+  ];
+
+queries['Personajes'] = 
+  [
+      // pregunta = imagen pintor, opcion = nombre pintor
+      `
+      SELECT ?optionLabel ?imageLabel
+      WHERE {
+          ?option wdt:P106 wd:Q1028181; 
+                  wdt:P18 ?imageLabel.        
+          SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }
+      }
+      LIMIT 200
+      `,
+      // pregunta = imagen futbolista, opcion = nombre futbolista
+      `
+      SELECT ?optionLabel ?imageLabel
+      WHERE {
+          ?option wdt:P106 wd:Q937857; 
+                      wdt:P18 ?imageLabel. 
+          SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }
+      }
+      LIMIT 200
+      `,
+      // pregunta = imagen cantante, opcion = nombre cantante
+      `
+      SELECT ?optionLabel ?imageLabel
+      WHERE {
+          ?option wdt:P106/wdt:P279* wd:Q177220; 
+                    wdt:P18 ?imageLabel.              
+          SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }
+      }
+      LIMIT 200
+      `
+  ];
 
 // Todas las preguntas, en el mismo orden que las consultas
 var questions = ["¿Que presidente de EE.UU es el que se muestra en la imagen?",
--- questiongenerator/text_questions.js
+++ questiongenerator/text_questions.js
@@ -1,45 +1,85 @@
 // Todas las consultas
-var queries = 
-    // pregunta = Pais, opcion = capitales
-    [`SELECT ?question ?questionLabel ?option ?optionLabel
-    WHERE {
-        ?question wdt:P31 wd:Q6256; wdt:P36 ?option.
-        SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }
-    }
-    LIMIT 200
-    `,
-    // pregunta = Club de futbol, opcion = estadio
-    `
-    SELECT ?question ?questionLabel ?option ?optionLabel
-    WHERE {
-        ?question wdt:P31 wd:Q476028.
-        ?question wdt:P115 ?option. 
-        ?question wdt:P17 wd:Q29.
-        SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }
-    }
-    LIMIT 100
-    `,
-    // Pregunta = Numero en la tabla periodica, opcion = Elemento
-    `
-    SELECT ?option ?optionLabel ?questionLabel (SUBSTR(?símbolo, 1, 1) AS ?sym)
-    WHERE {
-    ?option wdt:P31 wd:Q11344;        
-                wdt:P1086 ?questionLabel.        
-    SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE], es". }
-    }
-    LIMIT 100
-    `,
-    // Pregunta = Batalla historica, opcion = año
-    `SELECT ?question ?questionLabel ?optionLabel
-    WHERE {
-        ?question wdt:P31 wd:Q178561.    
-        ?question wdt:P580 ?date.      
-        FILTER (YEAR(?date) >= 1500 && YEAR(?date) <= 2000) 
-        BIND(YEAR(?date) as ?optionLabel)
-        SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }
-    }
-    LIMIT 100
-    `];
+var queries = {};
+
+queries['Geografia'] =
+    [
+        // pregunta = Pais, opcion = capitales
+        `SELECT ?question ?questionLabel ?option ?optionLabel
+        WHERE {
+            ?question wdt:P31 wd:Q6256; wdt:P36 ?option.
+            SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }
+        }
+        LIMIT 200
+        `,
+        // pregunta = pais, opcion = montaña
+        `
+        SELECT ?questionLabel ?optionLabel
+        WHERE {
+            ?country wdt:P31 wd:Q6256.             
+            ?country wdt:P610 ?mountainRange.     
+            ?mountainRange wdt:P2044 ?elevation.  
+            SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }
+        }
+        GROUP BY ?country ?questionLabel ?optionLabel
+        `,
+        // pregunta = poblacion, opcion = pais
+        `
+        SELECT ?optionLabel ?questionLabel
+        WHERE {
+            ?option wdt:P31 wd:Q6256.    
+            ?option wdt:P1082 ?questionLabel.  
+            SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }
+        }
+    `]
+
+queries['Cultura'] =
+    [
+        // pregunta = pelicula, opcion = director
+        `
+        SELECT DISTINCT ?questionLabel ?optionLabel
+        WHERE {
+            ?question wdt:P31 wd:Q11424;                 
+                    wdt:P57 ?option.                 
+            ?question wdt:P577 ?publicationDate.         
+            FILTER(YEAR(?publicationDate) >= 2000)   
+            SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es. }
+        }
+        LIMIT 200
+        `
+    ];
+
+queries['Informatica'] = 
+    [
+        // pregunta = software, opcion = fecha
+        `
+        SELECT ?questionLabel ?optionLabel
+        WHERE {
+            ?question wdt:P31 wd:Q7397.        
+            ?question wdt:P571 ?optionLabel.   
+            SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }
+        }
+        `,
+        // pregunta = sistema operativo, opcion = creador
+        `
+        SELECT ?questionLabel ?optionLabel
+        WHERE {
+            ?question wdt:P31 wd:Q9135.        
+            ?question wdt:P178 ?option.     
+            SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }
+        }
+        LIMIT 200
+        `,
+        // pregunta = creador, opcion = empresa
+        `
+        SELECT ?optionLabel ?questionLabel
+        WHERE {
+            ?option wdt:P31 wd:Q4830453.        
+            ?option wdt:P112 ?question.          
+            SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }
+        }
+        LIMIT 200
+        `
+    ];
 
 // Todas las preguntas, en el mismo orden que las consultas
 var questions = ["¿Cuál es la capital de ",
