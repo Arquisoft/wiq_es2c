e21336bad3282926900ea9ec70898b9b224b739a
Ruben <UO289337@uniovi.es>
2024-04-28 18:39:50 +0200

    Solucionado error visualizacion menu

--- webapp/src/components/fragments/MenuLateral.js
+++ webapp/src/components/fragments/MenuLateral.js
@@ -38,7 +38,11 @@ function MenuLateral() {
     };
 
     const showInicio = () => {
-        navigate(usernameGlobal === 'admin' ? "/PantallaInicioAdmin" : "/PantallaInicio");
+        if(usernameGlobal === 'admin'){
+            navigate("/PantallaInicioAdmin");
+        } else {
+            navigate("/PantallaInicio");
+        }
     };
 
     const showGameHistory = () => {
@@ -107,6 +111,76 @@ function MenuLateral() {
         </Box>
     );
 
+    const listaCompleta = () => (
+    <Box
+        role="presentation2"
+        onClick={toggleDrawer(false)}
+        onKeyDown={toggleDrawer(false)}
+        sx={{ width: 300,  display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center', }} // Aquí es donde estableces la anchura del Drawer
+        >
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', padding: '16px', mt: 5 }}>
+        <Avatar alt="Remy Sharp" src="../images/brain-icon2.ico" sx={{ marginRight: 2 }} />
+            <Typography variant="h6" sx={{ fontFamily: 'Arial', color: '#EE6D72', fontWeight: 'bold' }}>
+                BrainWIQ
+            </Typography>
+        </Box>
+        <List>
+            <ListItem>
+            <FormControl sx={{ m: 2, minWidth: 120 }}>
+            <Typography variant="h8" sx={{ fontFamily: 'Arial', color: '#EE6D72' }}>{t("idioma")}:</Typography>
+                <Select
+                    value={language}
+                    onChange={handleChange}
+                    displayEmpty
+                    inputProps={{ 'aria-label': 'Without label' }}
+                    MenuProps={{
+                        PaperProps: {
+                        style: {
+                            backgroundColor: '#f8b6bc', // Aquí puedes cambiar el color del menú
+                        },
+                        },
+                    }}
+                    sx={{ my: 2 }}
+                >
+                    <MenuItem value="" disabled >
+                        {t("seleccionar")}
+                    </MenuItem>
+                    <MenuItem value={'English'} onClick={() => i18n.changeLanguage('en')}>{t("ingles")}</MenuItem>
+                    <MenuItem value={'Spanish'} onClick={() => i18n.changeLanguage('es')}>{t("espanol")}</MenuItem>
+                </Select>
+                <MenuItem onClick={showGameHistory} sx={{ my: 2, fontFamily: 'Arial', color: '#EE6D72'  }}>
+                    <ListItemIcon>
+                        <HistoryIcon />
+                    </ListItemIcon>
+                    {t("toolHistorico")}
+                </MenuItem>
+                <MenuItem onClick={showRanking} sx={{ my: 2, fontFamily: 'Arial', color: '#EE6D72' }}>
+                    <ListItemIcon>
+                        <StarIcon />
+                    </ListItemIcon>
+                    {t("toolRanking")}
+                </MenuItem>
+                <MenuItem onClick={showPerfil} sx={{ my: 2, fontFamily: 'Arial', color: '#EE6D72' }}>
+                    <ListItemIcon>
+                        <AccountCircleIcon />
+                    </ListItemIcon>
+                    {t("toolPerfil")}
+                </MenuItem>
+                <MenuItem onClick={showLogout} sx={{ my: 2, fontFamily: 'Arial', color: '#EE6D72' }}>
+                    <ListItemIcon>
+                        <LogoutIcon />
+                    </ListItemIcon>
+                    {t("toolLogOut")}
+                </MenuItem>
+            </FormControl>
+            </ListItem>
+        </List>
+        </Box>
+    );
+
     if (location.pathname === '/App' || location.pathname === '/') {
         return (
             <div>
@@ -118,7 +192,7 @@ function MenuLateral() {
                 <Typography sx={{ flexGrow: 2, fontFamily: 'Arial', color: '#FFFFFF', fontWeight: 'bold', display: 'flex', justifyContent: 'center' }}>
                     BrainWIQ
                 </Typography>
-                {}
+                {/* Rest of your AppBar/Toolbar components */}
                 </Toolbar>
             </AppBar>
             <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
@@ -140,7 +214,7 @@ function MenuLateral() {
                 <Button onClick={showInicio} sx={{ flexGrow: 2, fontFamily: 'Arial', color: '#FFFFFF', fontWeight: 'bold', display: 'flex', justifyContent: 'center' }}>
                     BrainWIQ2
                 </Button>
-                {}
+                {/* Rest of your AppBar/Toolbar components */}
                 </Toolbar>
             </AppBar>
             <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
@@ -158,78 +232,17 @@ function MenuLateral() {
         <div>
         <AppBar position="static" style={{ backgroundColor: '#f8b6bc' }}>
             <Toolbar>
-            <IconButton edge="start" color="inherit" aria-label="menu3" onClick={() => toggleDrawer(true)}>
+            <IconButton edge="start" color="inherit" aria-label="menu3" onClick={toggleDrawer(true)}>
                 <MenuIcon />
             </IconButton>
-            <Button onClick={() => showInicio()} sx={{ flexGrow: 2, fontFamily: 'Arial', color: '#FFFFFF', fontWeight: 'bold', display: 'flex', justifyContent: 'center' }}>
+            <Button onClick={showInicio} sx={{ flexGrow: 2, fontFamily: 'Arial', color: '#FFFFFF', fontWeight: 'bold', display: 'flex', justifyContent: 'center' }}>
                 BrainWIQ
             </Button>
-            {}
+            {/* Rest of your AppBar/Toolbar components */}
             </Toolbar>
         </AppBar>
-        <Drawer anchor="left" open={drawerOpen} onClose={() => toggleDrawer(false)}>
-        <Box
-        role="presentation2"
-        onClick={() => toggleDrawer(false)}
-        onKeyDown={() => toggleDrawer(false)}
-        sx={{ width: 300,  display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center', }} // Aquí es donde estableces la anchura del Drawer
-        >
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', padding: '16px', mt: 5 }}>
-        <Avatar alt="Remy Sharp" src="../images/brain-icon2.ico" sx={{ marginRight: 2 }} />
-            <Typography variant="h6" sx={{ fontFamily: 'Arial', color: '#EE6D72', fontWeight: 'bold' }}>
-                BrainWIQ
-            </Typography>
-        </Box>
-            <List>
-                <ListItem>
-                <FormControl sx={{ m: 2, minWidth: 120 }}>
-                <Typography variant="h8" sx={{ fontFamily: 'Arial', color: '#EE6D72' }}>{t("idioma")}:</Typography>
-                    <Select
-                        value={language}
-                        onChange={() => handleChange()}
-                        displayEmpty
-                        inputProps={{ 'aria-label': 'Without label' }}
-                        MenuProps={{
-                            PaperProps: {
-                            style: {
-                                backgroundColor: '#f8b6bc', // Aquí puedes cambiar el color del menú
-                            },},}}
-                        sx={{ my: 2 }}>
-                        <MenuItem value="" disabled > {t("seleccionar")}</MenuItem>
-                        <MenuItem value={'English'} onClick={() => i18n.changeLanguage('en')}>{t("ingles")}</MenuItem>
-                        <MenuItem value={'Spanish'} onClick={() => i18n.changeLanguage('es')}>{t("espanol")}</MenuItem>
-                    </Select>
-                    <MenuItem onClick={() => showGameHistory()} sx={{ my: 2, fontFamily: 'Arial', color: '#EE6D72'  }}>
-                        <ListItemIcon>
-                            <HistoryIcon />
-                        </ListItemIcon>
-                        {t("toolHistorico")}
-                    </MenuItem>
-                    <MenuItem onClick={() => showRanking()} sx={{ my: 2, fontFamily: 'Arial', color: '#EE6D72' }}>
-                        <ListItemIcon>
-                            <StarIcon />
-                        </ListItemIcon>
-                        {t("toolRanking")}
-                    </MenuItem>
-                    <MenuItem onClick={() => showPerfil()} sx={{ my: 2, fontFamily: 'Arial', color: '#EE6D72' }}>
-                        <ListItemIcon>
-                            <AccountCircleIcon />
-                        </ListItemIcon>
-                        {t("toolPerfil")}
-                    </MenuItem>
-                    <MenuItem onClick={() => showLogout()} sx={{ my: 2, fontFamily: 'Arial', color: '#EE6D72' }}>
-                        <ListItemIcon>
-                            <LogoutIcon />
-                        </ListItemIcon>
-                        {t("toolLogOut")}
-                    </MenuItem>
-                </FormControl>
-                </ListItem>
-            </List>
-            </Box>
+        <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
+            {listaCompleta()}
         </Drawer>
         </div>
     );
