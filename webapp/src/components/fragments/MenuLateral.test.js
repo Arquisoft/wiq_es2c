8a60c762425454a66eb7198f8e0864b4ba2d860f
Ruben <UO289337@uniovi.es>
2024-04-28 17:54:40 +0200

    Nuevos test y cambio nombre dashboard Grafana

--- webapp/src/components/fragments/MenuLateral.test.js
+++ webapp/src/components/fragments/MenuLateral.test.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, act, waitFor } from '@testing-library/react';
 import MenuLateral from './MenuLateral'; // Asegúrate de que la ruta es correcta
 import { I18nextProvider } from "react-i18next";
 import i18n from "../../translations/i18n";
 import { UserProvider } from '../UserContext';
 import { MemoryRouter as Router, useLocation } from 'react-router-dom';
-
-
 
 i18n.changeLanguage("es");
 jest.mock('react-router-dom', () => ({
@@ -15,11 +13,53 @@ jest.mock('react-router-dom', () => ({
 }));
 
 describe('MenuLateral', () => {
-  test('renders AppBar', () => {
-    const { getByRole } = render(
+  it('renders the complete list', async() => {
+    const customLocation = { pathname: '/PantallaInicio', search: '', hash: '', state: null };
+    useLocation.mockReturnValue(customLocation);
+    const toggleDrawer = jest.fn();
+
+    const { getByRole, getByLabelText, getByText } = render(
       <I18nextProvider i18n={i18n}>
         <UserProvider>
-          <Router>
+          <Router initialEntries={['/']} initialIndex={0}>
+            <MenuLateral toggleDrawer={toggleDrawer}/>
+          </Router>
+        </UserProvider>
+      </I18nextProvider>
+    );
+
+    const iconButton = getByLabelText('menu3');
+
+    await act(() => {
+      fireEvent.click(iconButton);
+    }); 
+   
+    var boxElement;
+    await waitFor(() => {
+      boxElement = getByRole('presentation2');
+    });
+
+    await act(() => {
+      fireEvent.click(boxElement);
+      fireEvent.keyDown(boxElement, { key: 'Enter' }); 
+      fireEvent.keyDown(boxElement, { key: 'Escape' });
+
+      fireEvent.click(getByText('Historico'));
+      fireEvent.click(getByText('Ranking'));
+      fireEvent.click(getByText('Perfil'));
+      fireEvent.click(getByText('Cerrar sesión'));
+  
+    });
+  });
+  
+  it('renders MenuLateral', async() => {
+    const customLocation = { pathname: '/', search: '', hash: '', state: null };
+    useLocation.mockReturnValue(customLocation);
+
+    const { getByRole, getByText } = render(
+      <I18nextProvider i18n={i18n}>
+        <UserProvider>
+          <Router initialEntries={['/']} initialIndex={0}>
             <MenuLateral />
           </Router>
         </UserProvider>
@@ -27,25 +67,30 @@ describe('MenuLateral', () => {
     );
     const appBar = getByRole('banner');
     expect(appBar).toBeInTheDocument();
+
+    await act(() => {
+      const menuButton = getByText('BrainWIQ');
+      fireEvent.click(menuButton);
+    });
   });
 
-  test('muestra la barra de navegacion correctamente en /', async () => {
-    const customLocation = { pathname: '/', search: '', hash: '', state: null };
+  it('muestra la barra de navegacion correctamente en /', async () => {
+    const customLocation = { pathname: '/Game', search: '', hash: '', state: null };
     useLocation.mockReturnValue(customLocation);
-    const { getByRole, getByLabelText } = render(
+
+    const { getByText } = render(
       <I18nextProvider i18n={i18n}>
         <UserProvider>
-          <Router>
+          <Router initialEntries={['/']} initialIndex={0}>
             <MenuLateral />
           </Router>
         </UserProvider>
       </I18nextProvider>
     );
-    const menuButton = getByLabelText('menu');
-    fireEvent.click(menuButton);
-    const drawer = getByRole('presentation');
-    expect(drawer).toBeInTheDocument();
-  });
 
-  // Añade más pruebas según sea necesario
+    await act(() => {
+      const menuButton = getByText('BrainWIQ2');
+      fireEvent.click(menuButton);
+    });
+  });
 });
